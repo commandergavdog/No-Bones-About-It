968bf09c97b7435c01ea32a0f495a551f0d0ccf8
Frank Dominguez <fdominguez@naic.org>
2023-03-24 23:59:05 -0500

    update logic.js

--- logic.js
+++ logic.js
@@ -1,24 +1,37 @@
-// function openModal() {
-    // modalBackground.setAttribute("close");
-// }
+
+
+function openModal() {
+    window.onload = function() {
+        setTimeout(function(){
+            let modalContainer = document.querySelector(".modal-container");
+            // console.log(modalContainer)
+            modalContainer.setAttribute("open", "true");
+        }, 1000)
+    }
+}
 
 function closeModal() {
 
-    let modalBackground = document.querySelector(".modal-container");
-    let modalCloser = document.getElementsByTagName("i")[1];
-
-    modalCloser.onclick = function(){        
-        modalBackground.removeAttribute("open");    
-    };
-
-    modalBackground.onclick = function(){
-        console.log("MB clicked")
-    };
-
-    document.body.onclick = function(){
-        console.log("body clicked")
-    };
+    document.addEventListener(
+        "click",
+        function (event) {
+            // If user either clicks X button OR clicks outside the modal window, then close modal by calling closeModal()
+            if (
+                event.target.matches(".mc") ||
+                !event.target.closest(".modal")
+            ) {
+                closeModal()
+            }
+        },
+        false
+    )
+    
+    function closeModal() {
+        document.querySelector(".modal-container").style.display = "none"
+    }
 
 }
 
 closeModal();
+openModal();
+
